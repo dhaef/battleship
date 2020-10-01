93dc0dad6f8882d0a3655aa80314ab5855f6fe5c
Derek <derekhaefner@gmail.com>
2020-10-01 19:33:52 -0400

    Add newGame function to start a new game

--- src/App.js
+++ src/App.js
@@ -4,31 +4,30 @@ import Board from './gameboard/Board';
 import Gameboard from './gameboard/Gameboard';
 import Player from './player/Player';
 import AddShips from './AddShips';
-
-const user = {
-  player: Player('User'),
-  gameboard: Gameboard()
-}
-const bot = {
-  player: Player('bot'),
-  gameboard: Gameboard()
-}
+import Alert from './Alert';
 
 function App() {
-  const [userShips, setUserShips] = useState([]);
-  const [botShips, setBotShips] = useState([]);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [user, setUser] = useState({
+    player: Player('User'),
+    gameboard: Gameboard()
+  });
+  const [bot, setBot] = useState({
+    player: Player('bot'),
+    gameboard: Gameboard()
+  });
   const [userMoves, setUserMoves] = useState({ hits: [], misses: [] });
   const [botMoves, setBotMoves] = useState({ hits: [], misses: [] });
+  const [isAlert, setIsAlert] = useState({ value: false, msg: null, type: null });
 
   const startGame = () => {
-    setUserShips(user.gameboard.getShips());
+    setGameStarted(true);
     bot.gameboard.addShip(3, { x: 1, y: 1, direction: 'down' });
     bot.gameboard.addShip(4, { x: 1, y: 2, direction: 'down' });
     bot.gameboard.addShip(5, { x: 1, y: 3, direction: 'down' });
     // bot.gameboard.addRandomShip(3);
     // bot.gameboard.addRandomShip(4);
     // bot.gameboard.addRandomShip(5);
-    setBotShips(bot.gameboard.getShips());
   }
 
   const handlePlayerClick = (coordinates) => {
@@ -36,30 +35,48 @@ function App() {
       bot.gameboard.receiveAttack(coordinates);
       setBotMoves({ hits: bot.gameboard.getHits(), misses: bot.gameboard.getMisses() });
     } catch (error) {
-      console.log('You already tried that spot!');
-      return 'You already tried that spot!'
+      setIsAlert({ value: true, msg: error.message, type: 'basic' });
+      return
     }
 
     if (bot.gameboard.allSunk()) {
-      console.log('Player wins')
-      return 'Player 1 wins'
+      setIsAlert({ value: true, msg: `${user.player.getName()} wins!`, type: 'gameover' });
+      return
     } else {
       user.gameboard.receiveAttack();
       setUserMoves({ hits: user.gameboard.getHits(), misses: user.gameboard.getMisses() });
       if (user.gameboard.allSunk()) {
-        console.log('Bot wins')
-        return 'Bot wins'
+        setIsAlert({ value: true, msg: `${bot.player.getName()} wins!`, type: 'gameover' });
+        return
       }
     }
   }
 
+  const newGame = () => {
+    setGameStarted(false);
+    setUser({
+      player: Player('User'),
+      gameboard: Gameboard()
+    });
+    setBot({
+      player: Player('bot'),
+      gameboard: Gameboard()
+    });
+    setUserMoves({ hits: [], misses: [] });
+    setBotMoves({ hits: [], misses: [] });
+    console.log('New Game');
+  }
+
   return (
     <div>
-      {userShips.length > 0 && <Board player={user} moves={userMoves} />}
-      {userShips.length > 0 && <Board player={bot} moves={botMoves} handlePlayerClick={handlePlayerClick} />}
-      {userShips.length === 0 && <AddShips
+      <h1>Battleship</h1>
+      {isAlert.value && <Alert message={isAlert.msg} setAlert={setIsAlert} newGame={newGame} type={isAlert.type} />}
+      {gameStarted && <Board player={user} moves={userMoves} />}
+      {gameStarted && <Board player={bot} moves={botMoves} handlePlayerClick={handlePlayerClick} />}
+      {!gameStarted && <AddShips
         gameboard={user.gameboard}
         startGame={startGame}
+        setAlert={setIsAlert}
       />}
     </div>
   );
